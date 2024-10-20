9867ed465d94f5375cce39b74b26fe2ebea39879
Deva <dvrharsha@gmail.com>
2024-10-20 13:12:09 -0400

    updated

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,10 +10,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.46.0"
+      version = "=4.1.0"
     }
   }
-  required_version = "~>1.0.0"
+  required_version = "~>1.6.4"
   backend "azurerm" {}
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-str-29"
+    storage_account_name = "tfstrbck29"
+    container_name       = "terraform-state"
+    key                  = "rg.terraform.tfstate" 
+  }
+}
